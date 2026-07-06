810133bf7de72824fbe1752396b61909526afb31
sanjeet kumar <sumitkumar989159@gmail.com>
2026-07-06 23:31:25 +0530

    Add files via upload

---
+++ Pandas/Grouping/grouping.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie", "David"],
+    "Age": [25, 30, 22, 30],
+    "Salary": [50000, 60000, 45000, 70000],
+    "City": ["Delhi", "Mumbai", "Delhi", "Chennai"]
+}
+
+df = pd.DataFrame(data)
+
+print("Original DataFrame:")
+print(df)
+print(df.groupby("City")["Salary"].mean())
